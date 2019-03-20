2ddfeda19c74d91af473d452f252f273e2247bd7
samtech777 <43475772+samtech777@users.noreply.github.com>
2019-03-20 16:59:32 +0000

    Add files via upload

---
+++ t1.py
@@ -0,0 +1,28 @@
+print("This Is Testing")
+
+
+A=10000
+B=3000
+
+print(A+B)
+
+print("Add A + B :", A+B)
+
+print("Multiply A x B :", A*B)
+
+print("Sub A - B :", A-B)
+
+print("Sub B - A :", B-A)
+
+print("DIV A / B :", A/B)
+
+print(" % B", B*100/A)
+
+c=input("Please enter your name:")
+
+print("My Name is :", c)
+
+
+d=eval(input("Please enter 2 number e.g 7*3 or 7+3 :"))
+
+print(" The Result is :", d)
